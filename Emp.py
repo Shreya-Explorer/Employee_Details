5852e56ea49cfce18b47d5ffeea2dcfa7bacfa61
Shreya Raykar <shreyaraikar2304@gmail.com>
2025-11-13 17:29:04 +0530

    Update Emp.py

--- Emp.py
+++ Emp.py
@@ -1,6 +1,6 @@
-Name:"Shreya"
-Salary:1500000
-Address:"Sirsi"
-print("Employee Name:", Name)
-print("Employee Salary:", Salary)
-print("Employee Address:", Address)
+name:"Shreya"
+salary:1500000
+address:"Sirsi"
+print("Employee Name:", name)
+print("Employee Salary:", salary)
+print("Employee Address:", address)
